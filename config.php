6599b5eb2883a2748119c863a4d62c5a68cae0ee
Linuz <dennismald@gmail.com>
2012-12-21 23:24:06 -0600

    cleanup

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 	$DB_HOST = "localhost";
 	$DB_PORT = "3306";
-	$DB_NAME = "";
-	$DB_USER = "";
-	$DB_PASS = "";
+	$DB_NAME = "cougarcs_test";
+	$DB_USER = "cougarcs_test";
+	$DB_PASS = "cougarcs_test";
 	
 	$DB_HOST = $DB_HOST.":".$DB_PORT;
 ?>
